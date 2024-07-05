0de8eb4dc5e40c78cf2f15c792e7bbcfe6de1265
khan2626 <israelkhan217@gmail.com>
2024-07-05 10:57:47 +0100

    structured the front-end

---
+++ my-app/src/components/Assets/allProducts.js
@@ -0,0 +1,85 @@
+import p1_img from "./suheyl-burak-p7I07kuPSyU-unsplash.jpg";
+import p2_img from "./Lillusory.jpg";
+import p3_img from "./prettyGarden.jpg";
+import p4_img from "./Villiamcep.jpg";
+import p5_img from "./w_gown.jpg";
+import p6_img from "./m_BomberJacket.jpg";
+import p7_img from "./m_DesertChuckerBoot.jpg";
+import p8_img from "./m_ZipUpCoat.jpg";
+import p9_img from "./m_Oxford.jpg";
+import p10_img from "./md-salman-tWOz2_EK5EQ-unsplash.jpg";
+
+let allProduct = [
+  {
+    id: 1,
+    name: "Lillusory exquisite gown",
+    category: "women",
+    image: p2_img,
+    newPrice: 50.0,
+    oldPrice: 80.5,
+  },
+  {
+    id: 2,
+    name: "Pretty Garden summer gown",
+    category: "women",
+    image: p3_img,
+    newPrice: 70.0,
+    oldPrice: 85.5,
+  },
+  {
+    id: 3,
+    name: "Villiamcep sleevless gown",
+    category: "women",
+    image: p4_img,
+    newPrice: 45.5,
+    oldPrice: 75.5,
+  },
+  {
+    id: 5,
+    name: "women exquisite summer gown",
+    category: "women",
+    image: p5_img,
+    newPrice: 55.0,
+    oldPrice: 90.5,
+  },
+  {
+    id: 6,
+    name: "Bomber Jacket for men",
+    category: "men",
+    image: p6_img,
+    newPrice: 50.0,
+    oldPrice: 80.5,
+  },
+  {
+    id: 7,
+    name: "Desert Chucker Boot for men",
+    category: "men",
+    image: p7_img,
+    newPrice: 75.0,
+    oldPrice: 100.5,
+  },
+  {
+    id: 8,
+    name: "Zip Up Coat for men",
+    category: "men",
+    image: p8_img,
+    newPrice: 80.0,
+    oldPrice: 120.5,
+  },
+  {
+    id: 9,
+    name: "Oxford classy shoes for men",
+    category: "men",
+    image: p9_img,
+    newPrice: 50.0,
+    oldPrice: 80.5,
+  },
+  {
+    id: 10,
+    name: "round-neck T-shirts",
+    category: "kids",
+    image: p10_img,
+    newPrice: 50.0,
+    oldPrice: 80.5,
+  },
+];
--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,30 @@
-import React from "react";
-import './Navbar.css';
+import React, { useState } from "react";
 import { Icon } from '@iconify/react';
 import logo from '../Assets/ubestLogo.jpeg';
 
 export default function Navbar() {
+    const[menu, setMenu] = useState('shop');
+
     return (
-       <div className="navbar">
-        <div className="nav-logo">
-        <p>UBest</p>
-            <img src={logo} alt="logo" style={{ width: 110, height: 60 }}></img>
+    
+        <div className="w-full font-poppin py-3 shrink-0 shadow-lg flex mx-auto items-center justify-between px-2 sm:px-6 lg:px-8">
+        <div className="flex ">
+        <p className="font-bold text-3xl">UBest</p>
+            <img src={logo} alt="logo" style={{ width: 120, height: 70 }}></img>
             
         </div>
-        <ul className="nav-menu">
-            <li >Shop <hr/></li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+        <ul className="lg:flex md:flex hidden gap-3 sm:gap-5 cursor-pointer ">
+            <li onClick={()=>{setMenu('shop')}}>Shop{(menu==='shop')?<hr className="border-2 border-red-600"/>:<></>}</li>
+            <li onClick={()=>{setMenu('men')}}>Men{(menu==='men')?<hr className="border-2 border-red-600"/>:<></>}</li>
+            <li onClick={()=> {setMenu('women')}}>Women{(menu==='women')?<hr className="border-2 border-red-600"/>:<></>}</li>
+            <li onClick={()=> {setMenu('kids')}}>Kids{(menu==='kids')?<hr className="border-2 border-red-600"/>:<></>}</li>
         </ul>
-         <div className="login-cart">
-            <button>Login</button>
+         <div className="flex">
+            <button className="rounded-lg border-2 p-2 w-24 hover:bg-green-400">Login</button>
             <Icon icon="bytesize:cart" width="49px" height="49px" style={{color: '#e01b24'}} />
-            <div className="cart-count">0</div>
+            <div className=" text-red-700">0</div>
          </div>
-         
        </div>  
     )
 }
+
