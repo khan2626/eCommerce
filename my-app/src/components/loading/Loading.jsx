5c53d689cebefb714ceb6fa33353af8b8b5bf425
khan2626 <israelkhan217@gmail.com>
2025-02-09 15:31:01 +0100

    added loader

--- my-app/src/components/loading/Loading.jsx
+++ my-app/src/components/loading/Loading.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Loading = () => {
     return (
         <div className="flex gap-6 justify-center items-center h-screen">
-            <span className="loading loading-dots loading-lg"></span>
+            {/* <span className="loading loading-dots loading-lg"></span>
             <span className="loading loading-spinner text-primary"></span>
             <span className="loading loading-spinner text-secondary"></span>
             <span className="loading loading-spinner text-accent"></span>
-            <span className="loading loading-spinner text-neutral"></span>
+            <span className="loading loading-spinner text-neutral"></span> */}
             <span className="loading loading-spinner text-info"></span>
             <span className="loading loading-spinner text-success"></span>
             <span className="loading loading-spinner text-warning"></span>
